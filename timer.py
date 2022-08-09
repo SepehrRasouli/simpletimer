6148346e83174e20cb2d30370b94e60d1d972ead
SepehrRasouli <sepehrrs06@gmail.com>
2022-08-09 11:21:37 +0430

    Changed timer.py, seperated tui from main function.

--- timer.py
+++ timer.py
@@ -12,20 +12,30 @@ def _calculate_start_end(time_to_spend:int):
     end = start + timedelta(seconds=time_to_spend)
     return start,end
 
+def _show_tui(start,end,i,screen):
+    """
+    Shows TUI of timer app.
+    Args:
+        start,end: Datetime object
+        i: int, current second.
+        screen: rich screen object.
+    """
+    current = start + timedelta(seconds=i)
+    text = Align.center(
+    Text.from_markup(
+        f"""[blink]
+        Start: {start.hour}:{start.minute}:{start.second}\n 
+        Current: {current.hour}:{current.minute}:{current.second}\n
+        End: {end.hour}:{end.minute}:{end.second}[/blink]""",justify="center"
+        ),vertical="middle"
+    )
+    screen.update(text)
+
 def timer(remaining_time:int):
-    """ Time should be in minutes."""
+    """ Timer function."""
     remaining_time *= 60
     start,end = _calculate_start_end(remaining_time)
     with console.screen(style="bold #b2b2ff on #101010") as screen:
         for i in range(1,remaining_time+1):
-            current = start + timedelta(seconds=i)
-            text = Align.center(
-                Text.from_markup(
-                    f"""[blink]
-                    Start: {start.hour}:{start.minute}:{start.second}\n 
-                    Current: {current.hour}:{current.minute}:{current.second}\n
-                    End: {end.hour}:{end.minute}:{end.second}[/blink]""",justify="center"
-                ),vertical="middle"
-            )
-            screen.update(text)
+            _show_tui(start,end,i,screen)
             sleep(1)
